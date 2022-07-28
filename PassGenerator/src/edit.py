3913531ad7865021e151e7a014177c5300b4910e
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-07-28 10:54:41 +0200

    Update edit.py

--- PassGenerator/src/edit.py
+++ PassGenerator/src/edit.py
@@ -5,11 +5,12 @@ from tkinter import messagebox
 
 main_lst = []
 
-def edit(pathbase):
+def edit(pathbase, listbox1, list_of_services):
     window=Toplevel()
     window.title("Data Entry")
     window.geometry("450x250")
     def add():
+        global lst
         lst = [service.get(), mail.get(), username.get(), password.get()]
         main_lst.append(lst)
         messagebox.showinfo("Information","The data has been added successfully")
@@ -18,6 +19,9 @@ def edit(pathbase):
             Writer=csv.writer(f)
             Writer.writerows(main_lst)
             messagebox.showinfo("Information","Saved succesfully")
+        list_of_services.append(lst[0])
+        listbox1.insert(END, lst[0])
+
     def Clear():
             service.delete(0,END)
             mail.delete(0,END)
@@ -37,6 +41,7 @@ def edit(pathbase):
     add=Button(window,text="Add",padx=10,pady=10,command=add)
     Clear=Button(window,text="Clear",padx=10,pady=10,command=Clear)
 
+    
 
     label1.grid(row=0,column=0)
     label2.grid(row=1,column=0)
